4c7005a76f8272649e049de86d92bf1567aa76d9
ashu10832 <ashu10832@gmail.com>
2019-01-27 15:24:21 +0530

    Integrated DL Model

--- finger.py
+++ finger.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np 
 import copy
+import torch
+from torchvision import datasets
+import torchvision.transforms as transforms
+import pandas as pd
+from PIL import Image
+import PIL
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 
@@ -12,6 +20,42 @@ rect5_tl = (700,370)
 
 height = 30
 width = 30
+
+# define the CNN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer
+        self.conv1 = nn.Conv2d(1, 16,  3,padding = 1 )
+        #Convultion Sees 28*28*16
+        
+        self.conv2 = nn.Conv2d(16, 32, 3,padding = 1)
+        #convultion sees 14*14*32
+        
+        self.conv3 = nn.Conv2d(32, 64, 3,padding = 1)
+        #convultion sees 7*7*64
+        
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(64*3*3,256)
+        self.fc2 = nn.Linear(256,26)
+        self.dropout = nn.Dropout(0.25)
+        # max pooling layer
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1,64*3*3)
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        
+        return x
+
+
 
 def getHistogram(frame):
 	roi1 = frame[rect1_tl[1]:rect1_tl[1]+width,rect1_tl[0]:rect1_tl[0]+height]
@@ -113,9 +157,49 @@ def cropCharacter(canvas):
 
 	return canvas
 
+def getROI(canvas):
+	gray = cv2.bitwise_not(canvas)
+	ret, thresh = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY_INV)
+	_, ctrs, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	areas = []
+	for i in range(len(ctrs)):
+		x, y, w, h = cv2.boundingRect(ctrs[i])
+		areas.append((w*h,i))
 
+	def sortSecond(val): 
+		return val[0]  
+		
+	areas.sort(key = sortSecond,reverse = True)
+	x, y, w, h = cv2.boundingRect(ctrs[areas[1][1]])
+	cv2.rectangle(canvas, (x, y), (x + w, y + h), (255, 255, 0), 1)
+	roi = gray[y:y + h, x:x + w]
+	#cv2.imshow('ROI', roi)
+	return roi
 
+def predictCharacter(roi,model):
+	img = cv2.resize(roi, (28, 28)) 
+	img = cv2.GaussianBlur(img,(3,3),0)
+	img = Image.fromarray(img)
+	#img = PIL.ImageOps.invert(img)
 
+	normalize = transforms.Normalize(
+	   mean=[0.5,0.5,0.5],
+	   std=[0.5,0.5,0.5]
+	)
+	preprocess = transforms.Compose([
+	    transforms.Resize((28,28)),
+	    transforms.ToTensor(),
+	    normalize
+	])
+
+	p_img = preprocess(img)
+
+	model.eval()
+	p_img = p_img.reshape([1,1,28,28]).float()
+	output = model(torch.transpose(p_img,2,3))
+	_, preds_tensor = torch.max(output, 1)
+	preds = np.squeeze(preds_tensor.numpy())
+	return preds
 
 
 def main():
@@ -127,14 +211,17 @@ def main():
 
 	pressed = False
 	isDrawing = False
+	madePrediction = False
+	# create a complete CNN
+	model = Net()
+
+	model.load_state_dict(torch.load('model_emnist.pt',map_location='cpu'))
 	while True:
 		_ , frame = cap.read()
 		frame = cv2.flip(frame,flipCode = 1)
 		originalFrame = copy.deepcopy(frame)
 		originalFrame = drawRectangles(originalFrame)
 		canvas[:,:] = 255
-
-
 
 		key = cv2.waitKey(1)
 
@@ -147,6 +234,9 @@ def main():
 
 		if key & 0xFF == ord('c'):
 			canvas[:,:] = 255
+			isDrawing = False
+			far_points.clear()
+			madePrediction = False
 
 
 		if isDrawing:
@@ -154,14 +244,20 @@ def main():
 				far_points.pop(0)
 			far_points.append(far)
 			for i in range(len(far_points)-1):
-				cv2.line(originalFrame, far_points[i], far_points[i+1], (255,5,255), 10)
-				cv2.line(canvas, far_points[i], far_points[i+1], (0,0,0), 10)
+				cv2.line(originalFrame, far_points[i], far_points[i+1], (255,5,255), 20)
+				cv2.line(canvas, far_points[i], far_points[i+1], (0,0,0), 20)
 
 		if key & 0xFF == ord('f'):
 			isDrawing = False
 			#canvas = cropCharacter(canvas)
-			cv2.imshow('LeftCrop', canvas)
-			cv2.imwrite('Drawing.jpg', canvas)
+			#cv2.imshow('LeftCrop', canvas)
+			roi = getROI(canvas)
+			prediction = predictCharacter(roi, model)
+			print(prediction)
+			madePrediction = True
+			name = str(prediction) + '.jpg'
+			cv2.imwrite(name, roi)
+
 
 
 
@@ -178,8 +274,13 @@ def main():
 			
 
 
-			cv2.imshow('Mask', mask)
+			#cv2.imshow('Mask', mask)
 			#cv2.imshow('Canvas', canvas)
+
+		if madePrediction:
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			cv2.putText(originalFrame,'You wrote - ' + chr(prediction + 65),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+
 
 
 
